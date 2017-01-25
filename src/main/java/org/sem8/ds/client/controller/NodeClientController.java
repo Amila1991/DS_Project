71ae0b4bda887c121894b745b53711329e324ef0
amila1991 <amilakarunathilaka.12@cse.mrt.ac.lk>
2017-01-26 02:43:23 +0530

    implement node client controller

---
+++ src/main/java/org/sem8/ds/client/controller/NodeClientController.java
@@ -0,0 +1,39 @@
+package org.sem8.ds.client.controller;
+
+import org.sem8.ds.rest.resource.CommonResponseResource;
+import org.sem8.ds.rest.resource.GeneratedFileResponseResource;
+import org.sem8.ds.rest.resource.NodeResource;
+import org.sem8.ds.services.NodeService;
+import org.sem8.ds.services.exception.ServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @author amila karunathilaka.
+ */
+public class NodeClientController extends AbstractClientController {
+
+    @Autowired
+    NodeService nodeService;
+
+    public GeneratedFileResponseResource generateFiles(int noofFiles) throws ServiceException {
+        return nodeService.generateFileList(noofFiles);
+    }
+
+    public CommonResponseResource sendJoinRequest(NodeResource resource) throws ServiceException {
+        return nodeService.sendJoinRequest(resource);
+    }
+
+    public List<CommonResponseResource> sendJoinRequestAll() throws ServiceException {
+        return nodeService.sendJoinRequestAll();
+    }
+
+    public CommonResponseResource sendLeaveRequest(NodeResource resource) throws ServiceException {
+        return nodeService.sendLeaveRequest(resource);
+    }
+
+    public List<CommonResponseResource> sendLeaveRequestAll() throws ServiceException {
+        return nodeService.sendLeaveRequestAll();
+    }
+}
