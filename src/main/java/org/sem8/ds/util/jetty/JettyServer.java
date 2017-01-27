093c6402ab9b35511fde773100dba5c9aea223f2
amila1991 <amilakarunathilaka.12@cse.mrt.ac.lk>
2017-01-28 01:09:17 +0530

    implement ping method in jetty server

--- src/main/java/org/sem8/ds/util/jetty/JettyServer.java
+++ src/main/java/org/sem8/ds/util/jetty/JettyServer.java
@@ -3,40 +3,36 @@ package org.sem8.ds.util.jetty;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.sem8.ds.client.controller.BootstrapController;
 import org.sem8.ds.client.resource.RegisterResponseResource;
 import org.sem8.ds.rest.resource.NodeResource;
 import org.sem8.ds.services.exception.ServiceException;
+import org.sem8.ds.util.constant.NodeConstant;
 import org.springframework.web.context.ContextLoaderListener;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * @author amila karunathilaka.
  */
 public class JettyServer {
 
-    private  String warPath;
-    private  int serverPort;
+    private int serverPort;
 
-    private  Server server;
-
+    private Server server;
 
     public JettyServer(int serverPort) {
         setServerPort(serverPort);
         setup_web_xml(server);
-        this.warPath = warPath;
-        /*WebAppContext webApp = new WebAppContext();
-        webApp.setDescriptor("/WEB-INF/web.xml");
-        webApp.setResourceBase("src/main/webapp");
-        webApp.setContextPath("/");*/
-       // server.setHandler(webApp);
     }
 
     public void startServer() throws Exception {
-        if(!server.isStarted()) {
+        if (!server.isStarted()) {
             server.start();
         }
     }
@@ -62,10 +58,11 @@ public class JettyServer {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        jettyServer.jettyPing();
         BootstrapController bootstrapController = new BootstrapController();
         NodeResource resource = new NodeResource("localhost", 8082);
         try {
-            RegisterResponseResource response = bootstrapController.register(resource,"Amila");
+            RegisterResponseResource response = bootstrapController.register(resource, "Amila");
             System.out.println(response.getResponseType() + " " + response.getNode_No() + " " + response.getError());
 
         } catch (ServiceException e) {
@@ -75,7 +72,7 @@ public class JettyServer {
     }
 
     private void setup_web_xml (Server server) {
-        ServletContextHandler context = new ServletContextHandler(server,"/*", ServletContextHandler.SESSIONS);
+        ServletContextHandler context = new ServletContextHandler(server, "/*", ServletContextHandler.SESSIONS);
         ServletHolder servletHolder = new ServletHolder("jersey-servlet", new ServletContainer());
         servletHolder.setInitOrder(0);
         servletHolder.setInitParameter(ServerProperties.PROVIDER_PACKAGES, "org.sem8.ds.rest");
@@ -84,11 +81,17 @@ public class JettyServer {
         context.addEventListener(new ContextLoaderListener());
 
         context.setInitParameter("contextConfigLocation", "classpath:spring.xml");
+    }
 
+    public void jettyPing() {
+        Client client = ClientBuilder.newClient();
+        client.property(ClientProperties.CONNECT_TIMEOUT, 1000);
+        client.property(ClientProperties.READ_TIMEOUT, 1000);
+        String host;
+        host = NodeConstant.PROTOCOL + "localhost:" + serverPort + NodeConstant.REST_API;
+        WebTarget target = client.target(host).path(NodeConstant.NODE_SERVICE + "/check");
 
-
-       /* context.
-        context.;*/
+        Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
     }
 
 }
